bd96b5ce3d24114c26e65d52404642b3cda4d332
zhukreal <zhukreal@gmail.com>
2020-05-01 13:31:42 +0300

    Updates

---
+++ precache-manifest.1315ac34306fdf5a60f1bab3e65c7dc7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7c3658c3b9c8bae78d79f73f64fca6f",
+    "url": "/cashbox/index.html"
+  },
+  {
+    "revision": "1071533191de4ab35517",
+    "url": "/cashbox/static/css/main.4acfb673.chunk.css"
+  },
+  {
+    "revision": "279299f731797d5f37ef",
+    "url": "/cashbox/static/js/2.8ab81fc2.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/cashbox/static/js/2.8ab81fc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1071533191de4ab35517",
+    "url": "/cashbox/static/js/main.f8c772cc.chunk.js"
+  },
+  {
+    "revision": "3ccd7738a8e40371ebca",
+    "url": "/cashbox/static/js/runtime-main.a5459290.js"
+  },
+  {
+    "revision": "21bab41505b2f5832e5f9fbb038257d0",
+    "url": "/cashbox/static/media/Gilroyextrabold.21bab415.woff"
+  },
+  {
+    "revision": "1b3910514ea8e7d0f4613cb369002873",
+    "url": "/cashbox/static/media/Gilroylight.1b391051.woff"
+  },
+  {
+    "revision": "6f0efd9ee6e6541d4a5c42310a960de7",
+    "url": "/cashbox/static/media/bg-login-laptop.6f0efd9e.png"
+  },
+  {
+    "revision": "93c06e3d8e21859e119b59073c5c3442",
+    "url": "/cashbox/static/media/bg-login-mobile.93c06e3d.png"
+  },
+  {
+    "revision": "d56f2ef79b14de28b4b5fc521b90c254",
+    "url": "/cashbox/static/media/bg-login.d56f2ef7.png"
+  },
+  {
+    "revision": "044e2f218d02e3f4060574d76ebd5c98",
+    "url": "/cashbox/static/media/test.044e2f21.png"
+  }
+]);
