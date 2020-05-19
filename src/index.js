b00a03da28e5d005d3c86509e37e382bc071bb06
zhukreal <zhukreal@gmail.com>
2020-05-19 15:58:02 +0300

    icon logout

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import * as React from 'react'
 import * as ReactDom from 'react-dom'
 import { Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
-
 import { history } from './lib/routing'
 import { App } from './app'
 import { store } from './lib/store/store'
